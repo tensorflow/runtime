9cbf755d2877ad5f5cd7ba4a6e2a6085eb515aaf
TFRT team <tfrt-copybara-bot@google.com>
2020-08-10 14:00:47 -0700

    Remove TODO missed in previous commit
    PiperOrigin-RevId: 325882249

--- include/tfrt/host_context/kernel_registry.h
+++ include/tfrt/host_context/kernel_registry.h
@@ -38,8 +38,6 @@ class HostContext;
 
 // Kernel implementations use this signature, synchronously or asynchronously
 // performing some computation and updating results.
-//
-// TODO(b/160478052): Rename AsyncKernelFrame to AsyncKernelFrame.
 using AsyncKernelImplementation = void (*)(AsyncKernelFrame* frame);
 using SyncKernelImplementation = void (*)(SyncKernelFrame* frame);
 

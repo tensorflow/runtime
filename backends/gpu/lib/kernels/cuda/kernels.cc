a4cf1381265c3397e731dc72c1a864a938007559
TFRT team <tfrt-copybara-bot@google.com>
2020-09-22 15:02:29 -0700

    Implements cuda.module.load_static kernel.
    PiperOrigin-RevId: 333167364

--- backends/gpu/lib/kernels/cuda/kernels.cc
+++ backends/gpu/lib/kernels/cuda/kernels.cc
@@ -25,6 +25,7 @@
 #include "tfrt/dtype/dtype.h"
 #include "tfrt/gpu/memory/caching_gpu_allocator.h"
 #include "tfrt/gpu/memory/gpu_allocator.h"
+#include "tfrt/gpu/module_table.h"
 #include "tfrt/gpu/stream/cuda_wrapper.h"
 #include "tfrt/gpu/stream/stream_wrapper.h"
 #include "tfrt/gpu/tensor/dense_gpu_tensor.h"
@@ -36,18 +37,32 @@
 namespace tfrt {
 namespace cuda {
 
-// Convert 'error' to string and report to 'out'.
-static void ReportError(KernelErrorHandler out, llvm::Error error) {
+constexpr string_view kModuleTableResource = "cuda_module_table_resource";
+
+// Overloaded helpers for the REPORT_ERROR macro. Allows the macro to use either
+// strings or llvm::Errors.
+static void ReportErrorInternal(KernelErrorHandler error_handler,
+                                string_view error_message, string_view file,
+                                int line) {
+  return error_handler.ReportError(file, ':', line, ' ', error_message);
+}
+
+static void ReportErrorInternal(KernelErrorHandler error_handler,
+                                llvm::Error error, string_view file, int line) {
   llvm::handleAllErrors(std::move(error), [&](const llvm::ErrorInfoBase& info) {
-    out.ReportError(__FILE__, ':', __LINE__, ' ', info.message());
+    ReportErrorInternal(error_handler, info.message(), file, line);
   });
 }
+
+// Convert 'error' to string and report to 'error_handler'.
+#define REPORT_ERROR(error_handler, error) \
+  ReportErrorInternal(error_handler, error, __FILE__, __LINE__)
 
 // cuda.init initializes CUDA driver.
 static void CudaInit(Argument<Chain> in_chain, Result<Chain> out_chain,
                      KernelErrorHandler handler) {
   llvm::Error result = gpu::stream::Init(gpu::stream::Platform::CUDA);
-  if (result) return ReportError(handler, std::move(result));
+  if (result) return REPORT_ERROR(handler, std::move(result));
   out_chain.Set(in_chain);
 }
 
@@ -56,7 +71,7 @@ static void CudaDeviceGet(Argument<int32_t> ordinal, Argument<Chain> in_chain,
                           Result<gpu::stream::Device> out_device,
                           Result<Chain> out_chain, KernelErrorHandler handler) {
   auto device = gpu::stream::DeviceGet(gpu::stream::Platform::CUDA, *ordinal);
-  if (!device) return ReportError(handler, device.takeError());
+  if (!device) return REPORT_ERROR(handler, device.takeError());
   out_device.Emplace(device.get());
   out_chain.Set(in_chain);
 }
@@ -69,10 +84,10 @@ static void CudaStreamCreate(Argument<gpu::stream::Context> context,
                              Result<Chain> out_chain,
                              KernelErrorHandler handler) {
   auto current = gpu::stream::CtxSetCurrent(*context);
-  if (!current) return ReportError(handler, current.takeError());
+  if (!current) return REPORT_ERROR(handler, current.takeError());
   auto stream = gpu::stream::StreamCreate(
       *current, gpu::stream::StreamFlags::NON_BLOCKING);
-  if (!stream) return ReportError(handler, stream.takeError());
+  if (!stream) return REPORT_ERROR(handler, stream.takeError());
   out_stream.Emplace(std::move(*stream));
   out_chain.Set(in_chain);
 }
@@ -86,7 +101,7 @@ static void CudaStreamSynchronize(Argument<gpu::stream::OwningStream> stream,
                                   Result<Chain> out_chain,
                                   KernelErrorHandler handler) {
   llvm::Error error = gpu::stream::StreamSynchronize(stream->get());
-  if (error) return ReportError(handler, std::move(error));
+  if (error) return REPORT_ERROR(handler, std::move(error));
   out_chain.Set(in_chain);
 }
 
@@ -99,10 +114,10 @@ static void CudaEventCreate(Argument<gpu::stream::Context> context,
                             Result<Chain> out_chain,
                             KernelErrorHandler handler) {
   auto current = gpu::stream::CtxSetCurrent(*context);
-  if (!current) return ReportError(handler, current.takeError());
+  if (!current) return REPORT_ERROR(handler, current.takeError());
   auto event = gpu::stream::EventCreate(
       *current, gpu::stream::EventFlags::DISABLE_TIMING);
-  if (!event) return ReportError(handler, event.takeError());
+  if (!event) return REPORT_ERROR(handler, event.takeError());
   out_event.Emplace(std::move(*event));
   out_chain.Set(in_chain);
 }
@@ -116,7 +131,7 @@ static void CudaEventRecord(Argument<gpu::stream::OwningEvent> event,
                             KernelErrorHandler handler) {
   llvm::Error record_error =
       gpu::stream::EventRecord(event->get(), stream->get());
-  if (record_error) return ReportError(handler, std::move(record_error));
+  if (record_error) return REPORT_ERROR(handler, std::move(record_error));
   out_chain.Set(in_chain);
 }
 
@@ -130,7 +145,8 @@ static void CudaEventPoll(Argument<gpu::stream::OwningEvent> event,
                           AsyncKernelFrame* in_frame) {
   // TODO(b/146084342): Implement this with an efficient EventMgr.
   llvm::Error error = gpu::stream::EventSynchronize(event.get().get());
-  if (error) return ReportError(KernelErrorHandler(in_frame), std::move(error));
+  if (error)
+    return REPORT_ERROR(KernelErrorHandler(in_frame), std::move(error));
   out_chain.Set(in_chain);
 }
 
@@ -160,7 +176,7 @@ static void CudaMemAllocate(
     Argument<Chain> in_chain, Result<RCReference<gpu::GpuBuffer>> out_buffer,
     Result<Chain> out_chain, KernelErrorHandler handler) {
   auto buffer = (*allocator)->Allocate(*size, stream->get());
-  if (!buffer) return ReportError(handler, buffer.takeError());
+  if (!buffer) return REPORT_ERROR(handler, buffer.takeError());
   out_buffer.Emplace(std::move(*buffer));
   out_chain.Set(in_chain);
 }
@@ -186,7 +202,8 @@ static void CudaTensorMake(Argument<RCReference<gpu::GpuBuffer>> buffer,
                            Result<Chain> out_chain,
                            KernelErrorHandler handler) {
   if (!buffer.get()->IsValid()) {
-    handler.ReportError("Cannot make tensor from invalid (moved from?) buffer");
+    REPORT_ERROR(handler,
+                 "Cannot make tensor from invalid (moved from?) buffer");
     return;
   }
   if (buffer.get()->size() !=
@@ -196,7 +213,7 @@ static void CudaTensorMake(Argument<RCReference<gpu::GpuBuffer>> buffer,
     ss << "cuda.tensor.make failed: buffer_size (" << buffer.get()->size()
        << ") is not equal to the number of elements in shape (" << *shape
        << ") times element size (" << GetDType<T>().GetHostSize() << ")";
-    handler.ReportError(ss.str());
+    REPORT_ERROR(handler, ss.str());
     return;
   }
   tensor.Emplace(*shape, GetDType<T>(), std::move(*buffer));
@@ -222,27 +239,31 @@ static void CudaMemcpyHtoD(Argument<gpu::stream::Context> context,
                            Argument<Chain> in_chain, Result<Chain> out_chain,
                            KernelErrorHandler handler) {
   if (dst.get()->size() < *bytes_count) {
-    handler.ReportError(tfrt::StrCat(
-        "cuda.mem.copy_host_to_device failed: destination buffer size (",
-        dst.get()->size(), ") is less than number of bytes to copy (",
-        *bytes_count, ")"));
+    REPORT_ERROR(
+        handler,
+        tfrt::StrCat(
+            "cuda.mem.copy_host_to_device failed: destination buffer size (",
+            dst.get()->size(), ") is less than number of bytes to copy (",
+            *bytes_count, ")"));
     return;
   }
   if (src.get()->size() < *bytes_count) {
-    handler.ReportError(tfrt::StrCat(
-        "cuda.mem.copy_host_to_device failed: source buffer size (",
-        src.get()->size(), ") is less than number of bytes to copy (",
-        *bytes_count, ")"));
+    REPORT_ERROR(
+        handler,
+        tfrt::StrCat(
+            "cuda.mem.copy_host_to_device failed: source buffer size (",
+            src.get()->size(), ") is less than number of bytes to copy (",
+            *bytes_count, ")"));
     return;
   }
   auto current = gpu::stream::CtxSetCurrent(*context);
-  if (!current) return ReportError(handler, current.takeError());
+  if (!current) return REPORT_ERROR(handler, current.takeError());
   llvm::Error error =
       MemcpyAsync(*current, dst.get()->pointer(),
                   gpu::stream::Pointer<const void>(src.get()->data(),
                                                    gpu::stream::Platform::CUDA),
                   *bytes_count, stream->get());
-  if (error) return ReportError(handler, std::move(error));
+  if (error) return REPORT_ERROR(handler, std::move(error));
   out_chain.Set(in_chain);
 }
 
@@ -255,28 +276,72 @@ static void CudaMemcpyDtoH(Argument<gpu::stream::Context> context,
                            Argument<Chain> in_chain, Result<Chain> out_chain,
                            KernelErrorHandler handler) {
   if (dst.get()->size() < *bytes_count) {
-    handler.ReportError(tfrt::StrCat(
-        "cuda.mem.copy_device_to_host failed: destination buffer size (",
-        dst.get()->size(), ") is less than number of bytes to copy (",
-        *bytes_count, ")"));
+    REPORT_ERROR(
+        handler,
+        tfrt::StrCat(
+            "cuda.mem.copy_device_to_host failed: destination buffer size (",
+            dst.get()->size(), ") is less than number of bytes to copy (",
+            *bytes_count, ")"));
     return;
   }
   if (src.get()->size() < *bytes_count) {
-    handler.ReportError(tfrt::StrCat(
-        "cuda.mem.copy_device_to_host failed: source buffer size (",
-        src.get()->size(), ") is less than number of bytes to copy (",
-        *bytes_count, ")"));
+    REPORT_ERROR(
+        handler,
+        tfrt::StrCat(
+            "cuda.mem.copy_device_to_host failed: source buffer size (",
+            src.get()->size(), ") is less than number of bytes to copy (",
+            *bytes_count, ")"));
     return;
   }
   auto current = gpu::stream::CtxSetCurrent(*context);
-  if (!current) return ReportError(handler, current.takeError());
+  if (!current) return REPORT_ERROR(handler, current.takeError());
   llvm::Error error =
       MemcpyAsync(*current,
                   gpu::stream::Pointer<void>(dst.get()->data(),
                                              gpu::stream::Platform::CUDA),
 
                   src.get()->pointer(), *bytes_count, stream->get());
-  if (error) return ReportError(handler, std::move(error));
+  if (error) return REPORT_ERROR(handler, std::move(error));
+  out_chain.Set(in_chain);
+}
+
+static void CudaModuleLoadStatic(Argument<gpu::stream::Context> context,
+                                 Argument<Chain> in_chain,
+                                 Result<Chain> out_chain,
+                                 // Note: Attributes must be in alphabetical
+                                 // order (see b/140896071).
+                                 ArrayAttribute<int32_t> funcs_per_module,
+                                 AggregateAttr functions, AggregateAttr modules,
+                                 const ExecutionContext& exec_ctx,
+                                 KernelErrorHandler handler) {
+  auto current = gpu::stream::CtxSetCurrent(*context);
+  if (!current) return REPORT_ERROR(handler, current.takeError());
+
+  auto device = gpu::stream::CtxGetDevice(*current);
+  if (!device) return REPORT_ERROR(handler, device.takeError());
+
+  auto multi_device_module_table =
+      exec_ctx.resource_context()
+          ->GetOrCreateResource<std::unique_ptr<gpu::MultiDeviceModuleTable>>(
+              kModuleTableResource, gpu::MultiDeviceModuleTable::Create());
+  if (multi_device_module_table->get()->GetTable(*device).hasValue()) {
+    return REPORT_ERROR(handler,
+                        StrCat("Unable to load CUDA module table. Table has "
+                               "already been created for device ",
+                               device->id(current->platform())));
+  }
+  auto parsed_spec =
+      gpu::ParseModuleTableSpec(modules, funcs_per_module, functions);
+  if (!parsed_spec) return REPORT_ERROR(handler, parsed_spec.takeError());
+
+  auto module_table = gpu::ModuleTable::Create(*current, *parsed_spec);
+  if (!module_table) return REPORT_ERROR(handler, module_table.takeError());
+
+  if (auto error = multi_device_module_table->get()->AddTable(
+          *device, std::move(*module_table))) {
+    return REPORT_ERROR(handler, std::move(error));
+  }
+
   out_chain.Set(in_chain);
 }
 
@@ -317,6 +382,9 @@ void RegisterCudaKernels(KernelRegistry* kernel_reg) {
                         TFRT_KERNEL(CudaMemcpyHtoD));
   kernel_reg->AddKernel("cuda.mem.copy_device_to_host",
                         TFRT_KERNEL(CudaMemcpyDtoH));
+
+  kernel_reg->AddKernel("cuda.module.load_static",
+                        TFRT_KERNEL(CudaModuleLoadStatic));
 }
 
 }  // namespace cuda
---
+++ backends/gpu/mlir_tests/cuda/module.mlir
@@ -0,0 +1,108 @@
+// Copyright 2020 The TensorFlow Runtime Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// RUN: bef_executor $(bef_name %s) | FileCheck %s --dump-input=fail
+// RUN: tfrt_gpu_opt %s | tfrt_gpu_opt
+
+// CHECK-LABEL: --- Running 'module_load_static_test'
+func @module_load_static_test() {
+  %ch1 = tfrt.new.chain
+  %ch2 = cuda.init %ch1
+  %index = tfrt.constant.i32 0
+  %device, %ch3 = cuda.device.get %index, %ch2
+  %context, %ch4 = cuda_test.context.get %device, %ch2
+
+  // PTX for empty kernel.
+  // Typically module loading should be done at initialization time.
+  %ch5 = cuda.module.load_static %context, %ch4
+         { modules = [".version 6.0\n.target sm_35\n.address_size 64\n.visible .entry Kernel() { ret; }" ],
+           funcs_per_module = [1 : i32],
+           functions = [ "Kernel" ]
+         }
+  tfrt.return
+}
+
+// CHECK-LABEL: --- Running 'module_load_static_bad_ptx_test'
+func @module_load_static_bad_ptx_test() {
+  %ch1 = tfrt.new.chain
+  %ch2 = cuda.init %ch1
+  %index = tfrt.constant.i32 0
+  %device, %ch3 = cuda.device.get %index, %ch2
+  %context, %ch4 = cuda_test.context.get %device, %ch2
+
+  // expected-error @+1 {{CUDA_ERROR_INVALID_IMAGE}}
+  %ch5 = cuda.module.load_static %context, %ch4
+         { modules = ["This is bad!" ],
+           funcs_per_module = [1 : i32],
+           functions = [ "f" ]
+         }
+  tfrt.return
+}
+
+// CHECK-LABEL: --- Running 'module_load_static_neg_func_count_test'
+func @module_load_static_neg_func_count_test() {
+  %ch1 = tfrt.new.chain
+  %ch2 = cuda.init %ch1
+  %index = tfrt.constant.i32 0
+  %device, %ch3 = cuda.device.get %index, %ch2
+  %context, %ch4 = cuda_test.context.get %device, %ch2
+
+  // expected-error @+1 {{CUDA module table spec is malformed; Invalid function count (-1) specified for module 0}}
+  %ch5 = cuda.module.load_static %context, %ch4
+         { modules = [".version 6.0\n.target sm_35\n.address_size 64\n.visible .entry Kernel() { ret; }"],
+           funcs_per_module = [-1 : i32],
+           functions = [ "Kernel" ]
+         }
+  tfrt.return
+}
+
+// CHECK-LABEL: --- Running 'module_load_static_bad_func_count_test'
+func @module_load_static_bad_func_count_test() {
+  %ch1 = tfrt.new.chain
+  %ch2 = cuda.init %ch1
+  %index = tfrt.constant.i32 0
+  %device, %ch3 = cuda.device.get %index, %ch2
+  %context, %ch4 = cuda_test.context.get %device, %ch2
+
+  // expected-error @+1 {{CUDA module table spec is malformed; Number of entries in function count list doesn't match number of modules; 2 vs 1}}
+  %ch5 = cuda.module.load_static %context, %ch4
+         { modules = [".version 6.0\n.target sm_35\n.address_size 64\n.visible .entry Kernel() { ret; }" ],
+           funcs_per_module = [1 : i32, 2 : i32],
+           functions = [ "Kernel" ]
+         }
+  tfrt.return
+}
+
+// CHECK-LABEL: --- Running 'module_load_static_twice_test'
+func @module_load_static_twice_test() {
+  %ch1 = tfrt.new.chain
+  %ch2 = cuda.init %ch1
+  %index = tfrt.constant.i32 0
+  %device, %ch3 = cuda.device.get %index, %ch2
+  %context, %ch4 = cuda_test.context.get %device, %ch2
+
+  %ch5 = cuda.module.load_static %context, %ch4
+         { modules = [".version 6.0\n.target sm_35\n.address_size 64\n.visible .entry Kernel() { ret; }" ],
+           funcs_per_module = [1 : i32],
+           functions = [ "Kernel" ]
+         }
+
+  // expected-error @+1 {{Unable to load CUDA module table. Table has already been created for device 0}}
+  %ch6 = cuda.module.load_static %context, %ch5
+         { modules = [".version 6.0\n.target sm_35\n.address_size 64\n.visible .entry Kernel() { ret; }" ],
+           funcs_per_module = [1 : i32],
+           functions = [ "Kernel" ]
+         }
+  tfrt.return
+}

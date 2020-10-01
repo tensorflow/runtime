879d3aa39acc467f07865b28d2a677a76ad17ede
TFRT team <tfrt-copybara-bot@google.com>
2020-09-30 17:09:31 -0700

    Internal change
    PiperOrigin-RevId: 334710864

--- backends/gpu/lib/stream/blas_wrapper.cc
+++ backends/gpu/lib/stream/blas_wrapper.cc
@@ -19,16 +19,35 @@
 //===----------------------------------------------------------------------===//
 #include "tfrt/gpu/stream/blas_wrapper.h"
 
+#include "cublas.h"  // from @cuda_headers
 #include "llvm/Support/Errc.h"
 #include "llvm/Support/Error.h"
 #include "llvm/Support/FormatVariadic.h"
 #include "llvm/Support/raw_ostream.h"
 #include "tfrt/gpu/stream/cublas_wrapper.h"
+#include "tfrt/support/string_util.h"
 #include "wrapper_detail.h"
 
 namespace tfrt {
 namespace gpu {
 namespace stream {
+
+// Convert BLAS wrapper enums to cuBLAS enums.
+static constexpr auto ToCuda(BlasOperation operation) {
+  switch (operation) {
+    case BlasOperation::kNone:
+      return CUBLAS_OP_N;
+    case BlasOperation::kTranspose:
+      return CUBLAS_OP_T;
+    // CONJUGATE_TRANSPOSE and HERMITIAN_TRANSPOSE both have the same value
+    case BlasOperation::kConjugateTranspose:
+      return CUBLAS_OP_C;
+    case BlasOperation::kConjugate:
+      return CUBLAS_OP_CONJG;
+  }
+  llvm_unreachable(
+      StrCat("Unrecognized BlasOperation value: ", operation).c_str());
+}
 
 void internal::BlasHandleDeleter::operator()(BlasHandle handle) const {
   LogIfError(BlasDestroy(handle));
@@ -89,6 +108,23 @@ llvm::Error BlasSaxpy(CurrentContext current, BlasHandle handle, int n,
   switch (platform) {
     case Platform::CUDA:
       return CublasSaxpy(current, handle, n, alpha, x, incx, y, incy);
+    case Platform::ROCm:
+      return UnsupportedPlatform(platform);
+    default:
+      return InvalidPlatform(platform);
+  }
+}
+
+llvm::Error BlasSgemm(CurrentContext current, BlasHandle handle,
+                      BlasOperation transa, BlasOperation transb, int m, int n,
+                      int k, Pointer<const float> alpha, Pointer<const float> A,
+                      int lda, Pointer<const float> B, int ldb,
+                      Pointer<const float> beta, Pointer<float> C, int ldc) {
+  auto platform = handle.platform();
+  switch (platform) {
+    case Platform::CUDA:
+      return CublasSgemm(current, handle, ToCuda(transa), ToCuda(transb), m, n,
+                         k, alpha, A, lda, B, ldb, beta, C, ldc);
     case Platform::ROCm:
       return UnsupportedPlatform(platform);
     default:

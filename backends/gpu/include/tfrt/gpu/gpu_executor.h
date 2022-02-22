211aec63077ec8397b2821db11a5115a7964dbd0
Christian Sigg <csigg@google.com>
2022-02-22 10:29:57 -0800

    Remove outdated comment.
    PiperOrigin-RevId: 430242554

--- backends/gpu/include/tfrt/gpu/gpu_executor.h
+++ backends/gpu/include/tfrt/gpu/gpu_executor.h
@@ -68,9 +68,6 @@ struct BorrowedStreamDeleter {
   using pointer = AsyncValuePtr<GpuStream>;
   void operator()(pointer ptr);
 };
-// std::unique_ptr<AsyncValueRef<GpuStream>> wouldn't allow checking NumRef() on
-// destruction. Wrapping an AsyncValue* is sufficient because this type is only
-// used as BEF function argument.
 using BorrowedStream = std::unique_ptr<void, BorrowedStreamDeleter>;
 // Returns the `stream` belonging to `context` as a non-owning AsyncValue.
 BorrowedStream MakeBorrowedStream(AsyncValueRef<GpuContext> context,

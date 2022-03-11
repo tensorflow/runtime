2b2360dc589abf62dd1b89f475250806a4098f97
weihanmines <weihan13@amd.com>
2022-03-11 18:54:56 +0000

    remove commas in some json files

--- backends/gpu/tools/stub_codegen/generate.sh
+++ backends/gpu/tools/stub_codegen/generate.sh
@@ -20,8 +20,11 @@
 
 set -eux
 
+if [ "$#" -ne 1 ]; then
+  echo "Usage: root-tfrt/path/to/generate.sh <gcc|clang|msvc>"
+fi
 # Build the tools and generate the HIP header.
-bazel build --nocheck_visibility \
+bazel build --nocheck_visibility --config=$1\
   //backends/gpu/tools/stub_codegen:header_codegen \
   //backends/gpu/tools/stub_codegen:impl_codegen
 

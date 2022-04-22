46ca5043dd2f5fa7fb8f5a294a9f4896ae9313d9
Yilei Yang <yileiyang@google.com>
2022-04-21 19:38:19 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 443546914

--- mlir_tests/bef_perf/benchmark_utils.py
+++ mlir_tests/bef_perf/benchmark_utils.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# Lint as: python3
 """An util library that runs the input->mlir_to_bef->bef_executor pipeline."""
 
 from __future__ import absolute_import

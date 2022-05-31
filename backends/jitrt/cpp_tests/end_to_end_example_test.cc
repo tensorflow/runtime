d21470471cdcef17c311a69ae5b4d4474dcc1dc8
Eugene Zhulenev <ezhulenev@google.com>
2022-05-31 13:43:27 -0700

    [tfrt:jitrt] Add end-to-end example with specialization/recompilation and returning results
    PiperOrigin-RevId: 452130591

--- backends/jitrt/cpp_tests/end_to_end_example_test.cc
+++ backends/jitrt/cpp_tests/end_to_end_example_test.cc
@@ -21,11 +21,19 @@
 #include "benchmark/benchmark.h"
 #include "gtest/gtest.h"
 #include "llvm/ADT/SmallVector.h"
+#include "mlir/Conversion/TosaToLinalg/TosaToLinalg.h"
+#include "mlir/Dialect/Bufferization/Transforms/Bufferize.h"
+#include "mlir/Dialect/Bufferization/Transforms/Passes.h"
+#include "mlir/Dialect/Func/Transforms/Passes.h"
+#include "mlir/Dialect/Linalg/Passes.h"
+#include "mlir/Dialect/Tosa/IR/TosaOps.h"
+#include "mlir/Transforms/Passes.h"
 #include "tfrt/host_context/concurrent_work_queue.h"
 #include "tfrt/host_context/host_allocator.h"
 #include "tfrt/jitrt/jitrt.h"
 #include "tfrt/jitrt/jitrt_compiler.h"
 #include "tfrt/support/logging.h"
+#include "tfrt/tensor/dense_host_tensor.h"
 
 namespace tfrt {
 namespace jitrt {
@@ -36,38 +44,43 @@ using mlir::LogicalResult;
 using mlir::success;
 
 // Features supported in JitRt but missing in this example:
-//   1. Returning results from the compiled function.
-//   2. Launching async tasks.
-//   3. Returning async results from the compiled function.
+//   1. Launching async tasks.
+//   2. Returning async results from the compiled function.
 
 // TODO(ezhulenev): Show all the features supported by JitRt?
 
 // JitRt input program can be defined in arbitrary dialects, the only
 // requirement is that the user must pass a pipeline that can lower the input
 // program to the LLVM dialect (see `create_compilation_pipeline` option below).
+//
+// In this example we use Tosa to define the compute function body becase it's
+// available upstream, and transpose operation can showcase the input value
+// specialization: Tosa can lower to Linalg (and then to LLVM) only transpose
+// operations with constant permutation, without input value specialization this
+// program can't be lowered to LLVM and executed.
 static const char* mlir_module = R"(
   module {
     // Declare your own "runtime" intrinsics library in the compiled module.
     func.func private @my.runtime.intrinsic()
       attributes { rt.custom_call = "my.runtime.intrinsic" }
 
-    func.func @compute(%arg0: memref<?xf32>, %arg1: memref<?xf32>) {
+    // Permutation argument annotated with a jitrt constraint, which means that
+    // before compiling the function body, argument must be sunk into the
+    // function body as a constant. Otherwise tosa.transpose will not be lowered
+    // to Linalg operation.
+    func.func @compute(
+      %input: tensor<?x?xf32>,
+      %perm: tensor<2xi32> { jitrt.constraint = "value" }
+    ) -> tensor<?x?xf32> {
 
       // Pass attributes to the runtime intrinsics.
       func.call @my.runtime.intrinsic() { api_version = 1 : i32 } : () -> ()
 
-      // Host computation on buffers written as linalg op.
-      linalg.generic {
-        indexing_maps = [affine_map<(d0) -> (d0)>, affine_map<(d0) -> (d0)>],
-        iterator_types = ["parallel"]
-      }
-      ins(%arg0: memref<?xf32>) outs(%arg1 : memref<?xf32>) {
-        ^bb0(%in: f32, %out: f32):
-          %2 = arith.addf %in, %in : f32
-          linalg.yield %2 : f32
-      }
+      // Transpose input tensor and return result to the caller.
+      %transposed = "tosa.transpose"(%input, %perm)
+        : (tensor<?x?xf32>, tensor<2xi32>)  -> (tensor<?x?xf32>)
 
-      func.return
+      func.return %transposed : tensor<?x?xf32>
     }
   })";
 
@@ -103,46 +116,70 @@ TEST(EndToEndExampleTest, CompiledAndExecute) {
   // ------------------------------------------------------------------------ //
   CompilationOptions opts;
 
-  // Do not recompile executable for different symbolic shapes, because we do
-  // not set up specialization pipeline that can propagate symbolic shapes from
-  // operands to operations in the function body.
-  //
-  // For example for Tensorflow dialect shape propagation can canonicalize away
-  // some of the broadcasts or reshapes.
-  opts.specialization = CompilationOptions::Specialization::kDisabled;
+  // Because one of the arguments requires value specialization, we must enable
+  // specialization to be able to compile the executable.
+  opts.specialization = CompilationOptions::Specialization::kEnabled;
 
   // Define what dialects are supported in the input IR module. If you have your
   // own custom dialects in the input IR you must pass a callback that registers
-  // all the dialects that are considered legal for your inpuyt program.
-  opts.register_dialects = RegisterDefaultJitRtDialects;
+  // all the dialects that are considered legal for your input program.
+  //
+  // In this example in addition to "standard" JitRt dialects we add only Tosa.
+  opts.register_dialects = [](mlir::DialectRegistry& registry) {
+    registry.insert<mlir::tosa::TosaDialect>();
+    RegisterDefaultJitRtDialects(registry);
+  };
+
+  // Convert all tensors in the compute function signature to memrefs, because
+  // tensors do not have any runtime representation and can't be passed across
+  // the ABI boundary. The expectation is that compiler pipeline will act
+  // according to this calling convention, and the entrypoint will have the same
+  // function signature.
+  opts.calling_convention = CompilationOptions::DefaultCallingConvention(
+      mlir::bufferization::BufferizeTypeConverter());
 
   // ------------------------------------------------------------------------ //
   // 2. Set up compilation pipeline that lowers input module to LLVM.
   // ------------------------------------------------------------------------ //
 
-  // We rely on a default JitRt compilation pipeline, because we do not have
-  // any non "standard" dialects in this test.
+  // As a first step we lower from Tosa to Linalg on buffers, and then we rely
+  // on a default JitRt compilation pipeline to lower further to LLVM.
   opts.create_compilation_pipeline = [&](mlir::PassManager& pm) {
+    // 1. Lower Tosa to Linalg in tensors.
+    pm.addNestedPass<mlir::func::FuncOp>(mlir::tosa::createTosaToLinalg());
+
+    // 2. Lower Linalg on tensors to Linalg on buffers.
+    pm.addPass(mlir::func::createFuncBufferizePass());
+    pm.addNestedPass<mlir::func::FuncOp>(mlir::createLinalgBufferizePass());
+
+    // 3. Clean up IR after lowering to Linalg on buffers.
+    pm.addPass(mlir::createCSEPass());
+    pm.addPass(mlir::createCanonicalizerPass());
+
+    // 4. Continue compilation using the default JitRt pipeline.
     CompilationPipelineOptions copts;
     CreateDefaultJitRtCompilationPipeline(pm, copts);
   };
 
   // If your input IR requires specialization, you'll also need to define the
-  // `opts.create_compilation_pipeline` callback
+  // `opts.create_compilation_pipeline` callback. In this test we rely on the
+  // fact that "value-specialized" arguments will be materialized as constants
+  // in the function body.
 
   // ------------------------------------------------------------------------ //
   // 3. Instantiate JitExecutable from the input MLIR source.
   // ------------------------------------------------------------------------ //
 
   // JitExecutable does compilation/recompilation from the input source to the
-  // Executable artifact. In this particular example we always rely on default
-  // executable, because we didn't provide a specialization pipeline.
+  // Executable artifact.
   llvm::Expected<JitExecutable> jit_executable =
       JitExecutable::Instantiate(mlir_module, entrypoint, opts);
   if (auto err = jit_executable.takeError()) ASSERT_FALSE(err) << StrCat(err);
 
-  AsyncValuePtr<Executable> executable = jit_executable->DefaultExecutable();
-  Await(executable.value());  // make sure executable is available
+  // In this example default executable will be in error state, because the
+  // program requires value specialization and can't be compiled without it.
+  AsyncValuePtr<Executable> default_exec = jit_executable->DefaultExecutable();
+  ASSERT_TRUE(default_exec.IsError());
 
   // ------------------------------------------------------------------------ //
   // 4. Prepare input data for the compiled program.
@@ -155,13 +192,17 @@ TEST(EndToEndExampleTest, CompiledAndExecute) {
   // around.
 
   // Allocate storage for arguments.
-  std::vector<float> arg0 = {1.0, 2.0, 3.0, 4.0};
-  std::vector<float> arg1(4, 0.0);
+  std::vector<float> input = {1.0, 2.0, 3.0, 4.0};
+  std::vector<int32_t> perm = {1, 0};
+
+  // Input is a 2x2 memref.
+  std::array<int64_t, 2> sizes = {2, 2};
+  std::array<int64_t, 2> strides = {2, 1};
 
   // Prepare memref descriptors for the executable.
   llvm::SmallVector<MemrefDesc> args;
-  args.emplace_back(DType::F32, arg0.data(), 0, 4, 1);
-  args.emplace_back(DType::F32, arg1.data(), 0, 4, 1);
+  args.emplace_back(DType::F32, input.data(), 0, sizes, strides);
+  args.emplace_back(DType::I32, perm.data(), 0, 2, 1);
 
   // ------------------------------------------------------------------------ //
   // 5. Prepare options for executing the JitRt executable.
@@ -181,24 +222,61 @@ TEST(EndToEndExampleTest, CompiledAndExecute) {
   execute_opts.custom_call_data = &user_data;
 
   // ------------------------------------------------------------------------ //
-  // 6. Define how to convert returned values back to C++ objects.
+  // 6. Get executable specialized for the concrete operands.
   // ------------------------------------------------------------------------ //
 
-  // TODO(ezhulenev): This is a very boring example that doesn't return
-  // anything. Make it more interesting if someone is curious to see how
-  // returning values will look like.
-  NoOpReturnValueConverter converter;
+  // At this point we trigger compilation of the original input program for
+  // the concrete value of the transpose permutation vector.
+  llvm::Expected<AsyncValuePtr<Executable>> executable =
+      jit_executable->GetExecutable(args);
+
+  // Await the successful compilation completion.
+  if (auto err = executable.takeError()) ASSERT_FALSE(err) << StrCat(err);
+  Await(executable->value());
 
   // ------------------------------------------------------------------------ //
-  // 7. Call JitRt executable with the prepared operands.
+  // 7. Define how to convert returned values back to C++ objects.
+  // ------------------------------------------------------------------------ //
+
+  // Conversion context allows to pass data from the caller to the result
+  // conversion function (e.g. auxiliary data structures to distinguish newly
+  // allocated memrefs from forwarded arguments). In this example we don't pass
+  // anything to the conversion functions.
+  struct ResultConversionCtx {};
+  ResultConversionCtx conversion_ctx;
+
+  // TODO(ezhulenev): We should decouple JitRt from TFRT specific
+  // RemainingResults, and do not force clients to deal with returned
+  // AsyncValues.
+
+  // Placeholders for returned values.
+  unsigned num_results = (*executable)->num_results();
+  llvm::SmallVector<RCReference<AsyncValue>> result_values(num_results);
+  RemainingResults results(result_values);
+
+  // If execution failed errors will be automatically allocated for all results.
+  ReturnValueConverter<ResultConversionCtx> converter(results, conversion_ctx);
+  converter.AddConversion(ReturnMemrefAsDenseHostTensor<ResultConversionCtx>);
+
+  // ------------------------------------------------------------------------ //
+  // 8. Call JitRt executable with the prepared operands.
   // ------------------------------------------------------------------------ //
 
   // Execute Jit compiled executable.
-  ASSERT_FALSE(executable->Execute(args, converter, execute_opts));
+  auto err = (*executable)->Execute(args, converter, execute_opts);
+  ASSERT_FALSE(err) << "Failed to execute: " << StrCat(err);
 
-  // Check that compute function produced the correct results.
-  auto expected = llvm::map_range(arg0, [](float a) { return a + a; });
-  EXPECT_EQ(arg1, std::vector<float>(expected.begin(), expected.end()));
+  // Check the result returned from the compiled function.
+  ASSERT_TRUE(result_values[0]->IsAvailable());
+
+  // Result must be a DenseHostTensor.
+  auto& result_tensor = result_values[0]->get<tfrt::DenseHostTensor>();
+  ASSERT_EQ(result_tensor.dtype(), DType::F32);
+  ASSERT_EQ(result_tensor.NumElements(), 4);
+
+  ArrayRef<float> data = {result_tensor.data<float>(), 4};
+  std::vector<float> expected = {1.0, 3.0, 2.0, 4.0};
+  EXPECT_EQ(data.vec(), std::vector<float>(expected.begin(), expected.end()));
 
   // ------------------------------------------------------------------------ //
   // 8. Saving/Restoring JitRt executable to/from object file.
